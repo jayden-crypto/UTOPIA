0d1b15c81b947b750367d77dddeeaac9de1a1ab2
jayden-crypto <135226758+jayden-crypto@users.noreply.github.com>
2023-08-17 22:59:57 +0530

    Update froshbot.py

--- froshbot/froshbot.py
+++ froshbot/froshbot.py
@@ -1,61 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+import cmath
 
-def automate_event_booking(username, password):
-    # Set up the WebDriver (provide the path to chromedriver)
-    driver = webdriver.Chrome(executable_path='path_to_chromedriver.exe')
+a = float(input("Enter the coefficient of x^2: "))
+b = float(input("Enter the coefficient of x: "))
+c = float(input("Enter the constant: "))
 
-    try:
-        # Open the college event booking website
-        driver.get('https://college-events-example.com')
+disc = b**2 - 4*a*c
 
-        # Login
-        username_field = driver.find_element_by_id('username')
-        password_field = driver.find_element_by_id('password')
-        login_button = driver.find_element_by_id('login-button')
+root1 = (-b - cmath.sqrt(disc)) / (2*a)
+root2 = (-b + cmath.sqrt(disc)) / (2*a)
 
-        username_field.send_keys(username)
-        password_field.send_keys(password)
-        login_button.click()
-
-        # Navigate to the events page
-        events_page_link = driver.find_element_by_link_text('Events')
-        events_page_link.click()
-
-        # Find all available events
-        event_buttons = driver.find_elements_by_xpath('//div[@class="event"]/button')
-
-        # Loop through events and book them
-        for event_button in event_buttons:
-            event_name = event_button.text
-            event_button.click()
-
-            # Fill in additional details (ticket count)
-            ticket_count_input = driver.find_element_by_id('ticket-count')
-            ticket_count_input.send_keys('2')
-
-            # Proceed to payment/confirmation
-            proceed_button = driver.find_element_by_id('proceed-button')
-            proceed_button.click()
-
-            # Complete the booking process
-            confirm_button = driver.find_element_by_id('confirm-button')
-            confirm_button.click()
-
-            # Wait for a moment before going back to events page
-            time.sleep(5)
-
-            # Go back to events page
-            driver.back()
-
-    finally:
-        # Close the browser, regardless of success or failure
-        driver.quit()
-
-# Replace these values with your actual credentials
-username = 'your_username'
-password = 'your_password'
-
-# Call the function to automate event booking
-automate_event_booking(username, password)
+if disc == 0 or disc > 0:
+    print(f"The roots are {root1} and {root2}")
+elif disc < 0:
+    Croot1 = cmath.sqrt(-disc) / (2*a)
+    Croot2 = cmath.sqrt(-disc) / (2*a)
+    Rroot1 = -b / (2*a)
+    Rroot2 = -b / (2*a)
+    print(f"The first root is {Rroot1} - i{Croot1}")
+    print(f"The second root is {Rroot2} + i{Croot2}")
